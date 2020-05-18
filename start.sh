77040ec47b6498d23de8e76d30d50d997869305e
xtuyul <chamot6@Gmail.com>
2020-05-19 04:18:50 +0700

    #

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
+python bot.py +[phone1] doge
 python bot.py +[phone1] zec
 python bot.py +[phone1] ltc
-python bot.py +[phone1] doge
 python bot.py +[phone1] btc
 python bot.py +[phone1] bch
 clear
 pwd
 bash start.sh
 
-#Replace [phone] with your Number Phone are registered with ClickBot
+#Replace [phone] with your Number Phone
+#ext 
+   python bot.py +62xxxx doge
